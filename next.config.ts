73e99e93d22a40001e0fbc840067b8ece123dc16
Sumit-72 <162539584+Sumit-72@users.noreply.github.com>
2025-01-11 11:39:25 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+    typescript: {
+        // Ignore TypeScript errors during build
+        ignoreBuildErrors: true,
+    },
 };
 
 export default nextConfig;
